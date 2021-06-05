5896eafddd36cffb93da847fd77661a669a69b84
Wagner Xu <wagnerhsu@hotmail.com>
2021-06-05 14:37:23 +0800

    Update version

--- Bonobo.Git.Server/Properties/AssemblyInfo.cs
+++ Bonobo.Git.Server/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Web;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("6.2.0.0")]
-[assembly: AssemblyFileVersion("6.2.0.0")]
+[assembly: AssemblyFileVersion("6.2.1.0")]
--- Bonobo.Git.Server/README.History.md
+++ Bonobo.Git.Server/README.History.md
@@ -1,4 +1,7 @@
-﻿## 2019-07-24
+﻿## 2021-06-05
+### 6.2.1
+- Pull code from master and override all code
+## 2019-07-24
 
 - Update LogFilger to log CurrentExecutionFilePath
 - Comment `.blob img`
